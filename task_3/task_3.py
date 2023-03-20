e831fdf3e0a586dc4ce2addc1ea2792891bad21c
IGVP <igvp161280@gmail.com>
2023-03-20 21:24:22 +0700

    task_3_fin

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -8,3 +8,15 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+dictionary = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+with open('my_test_3.txt', 'r', encoding='UTF-8') as file_en:
+    text_en = file_en.read()
+
+text_ru = text_en
+
+for x, y in dictionary.items():
+    text_ru = text_ru.replace(x, y)
+
+with open('my_test_3_ru.txt', 'w', encoding='UTF-8') as file_ru:
+    file_ru.write(text_ru)
