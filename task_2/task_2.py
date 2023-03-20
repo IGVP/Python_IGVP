85d9cb3c89b4cb796c5208fee01edef4a732e157
IGVP <igvp161280@gmail.com>
2023-03-20 20:48:09 +0700

    Корректировка файлов

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -2,7 +2,7 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
-with open('my_test_2.xtx') as my_job:
+with open('my_test_2.txt') as my_job:
     line_cnt = 0
     world_cnt = 0
     for line in my_job:
