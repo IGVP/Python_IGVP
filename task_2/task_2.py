7e3173932690d528be7bfe687826c38b3960e1bd
IGVP <igvp161280@gmail.com>
2023-03-20 20:42:23 +0700

    task_2_fin

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -2,3 +2,12 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+with open('my_test_2.xtx') as my_job:
+    line_cnt = 0
+    world_cnt = 0
+    for line in my_job:
+        line_cnt += 1
+        world_cnt += len(line.split())
+        print(f'Строка №: "{line_cnt}", Кол-во слов с троке: "{len(line.split())}", текстк в строке : "{line}"')
+print(f'Количество строк: {line_cnt}')
+print(f'Количество слов во всех строках: {world_cnt}')
